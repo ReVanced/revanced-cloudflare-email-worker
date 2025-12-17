f0260ffd719199168b02ee3950bd9654342b40b5
madkarmaa <madkarma99@gmail.com>
2025-12-17 15:20:37 +0100

    refactor: remove unused imports

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,6 +1,4 @@
-import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
 import { describe, it, expect } from 'vitest';
-import worker from '../src/index';
 
 describe('worker', () => {
 	it('2 + 2 equals 4', async () => {
